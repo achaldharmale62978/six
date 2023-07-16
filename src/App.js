bd13d3f6e511a27546190652b38365041afdfb3c
achaldharmale62978 <achaldharmale62978@gmail.com>
2023-07-16 14:19:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Good Afternoon Everyone ......</h1>
+     <h2 id="f">Hello, <br/> i am java..</h2>
+     <button id='g'>submit</button>
+    </div>
+   
+  );
+}
+
+export default App;
